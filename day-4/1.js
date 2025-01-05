5a611e1dd4513e4c5b69a0728ccc7676fc250dea
Xievy <xuhvm3@gmail.com>
2025-01-05 19:03:37 +0530

    Backtracking

---
+++ day-4/1.js
@@ -0,0 +1,20 @@
+var permute = function(nums) {
+    
+    let res = [];
+    let n = nums.length
+
+    function helper(i) {
+        if ( i===n-1) {
+            res.push([...nums])
+            return;
+        }
+        for (let j=i; j<n; j++) {
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+            helper(i+1);
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+        }
+    
+}
+    helper(0);
+    return res;
+}
